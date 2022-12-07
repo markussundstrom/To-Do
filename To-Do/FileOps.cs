4d5e538945bce3d980040aa1759c6ea5dce93aaa
Markus Sundström <sundstrom.markus@gmail.com>
2022-12-07 11:42:35 +0100

    Initialized To-Do as separate repo

---
+++ To-Do/FileOps.cs
@@ -0,0 +1,31 @@
+﻿namespace To_Do
+{
+    public class FileOps
+    {
+        private string _file;
+
+        public FileOps()
+        {
+            _file = $"{Environment.CurrentDirectory}\\todo.json";
+        }
+
+        public string GetTaskFileContent()
+        {
+            if (File.Exists(_file))
+            {
+                using (StreamReader r = new StreamReader(_file))
+                {
+                    return r.ReadToEnd();
+                }
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+        public void PutTaskFileContent(string jsonstring)
+        {
+            File.WriteAllText(_file, jsonstring);
+        }
+    }
+}
---
+++ To-Do/TaskList.cs
@@ -0,0 +1,18 @@
+﻿namespace To_Do
+{
+    public class TaskList
+    {
+        public string Title { get; set; }
+        public List<Task> Tasks { get; set; }
+
+        public TaskList()
+        {
+        }
+
+        public TaskList(string title, List<Task> tasks)
+        {
+            Title = title;
+            Tasks = tasks;
+        }
+    }
+}
